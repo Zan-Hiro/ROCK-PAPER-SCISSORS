355fa4116681f014aa2fe554668aca945b7ac45b
Zan Hiro <zanhiro160@gmail.com>
2021-02-24 13:51:25 +0900

    Add five times game functionin game.js

--- javascript/game.js
+++ javascript/game.js
@@ -1,34 +1,91 @@
 'use strict';
 
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection === "Rock" && computerSelection === "Rock") {
-      console.log("Your selection is :" + playerSelection + " Computer Selection is : " + computerSelection + " So, Game is Draw!");
-    } else if(playerSelection === "Rock" && computerSelection === "Paper") {
-      console.log("Your selection is :" + playerSelection + " Computer Selection is : " + computerSelection + " So, You Lose!");
-    } else if(playerSelection === "Rock" && computerSelection === "Scissors"){
-      console.log("Your selection is :" + playerSelection + " Computer Selection is : " + computerSelection +  " So, You Win!");
-    } else if(playerSelection !== "Rock" || playerSelection !== "Paper" ||  playerSelection !== "Scissors"){
-        prompt("Please enter the words of 'Rock' or 'Paper' or 'Scissors'.\nUppercase Lowercase Capitalize all OK. \n But another words don't accept the game.");        
-      }
+alert("Hello, world.\nThis is Rock Paper Scissors Game on the console.");
+
+let playerTotalScore = 0;
+let computerTotalScore = 0;
+
+
+function game() {
+    const playerSelection = playerPlay();
+    const computerSelection = computerPlay();
+
+    //player choice funciton
+    function playerPlay() {
+      const playerChoice =  prompt("Please enter the words of 'Rock' or 'Paper' or 'Scissors'. \nUppercase Lowercase Capitalize all OK\nGame continues five times");
+      return capitalizeWords(playerChoice);
     }
+      
+        
+      //change the play choice of captalize
+      function capitalizeWords(str) {
+        let getCapitalizedSentence = str[0].toUpperCase() + str.slice(1).toLowerCase();
+        return getCapitalizedSentence;
+      }
+      
+      //computer choice function
+      function computerPlay() {
+        const computerChoice = ['Rock', 'Paper', 'Scissors'];
+        return computerChoice[Math.floor(Math.random()*computerChoice.length)];
+      }
+      
+      //game round function only one time
+      function playRound(playerSelection, computerSelection) {
+        let playerScore = 0;
+        let computerScore = 0;
+          if(playerSelection === "Rock" && computerSelection === "Rock" || playerSelection === "Paper" && computerSelection === "Paper" || playerSelection === "Scissors" && computerSelection === "Scissors" ) {
+            console.log("Your selection is :" + playerSelection + " Computer Selection is : " + computerSelection + " So, Game is Draw!");
+          } else if(playerSelection === "Rock" && computerSelection === "Paper" || playerSelection === "Paper" && computerSelection === "Scissors" || playerSelection === "Scissors" && computerSelection === "Rock") {
+            computerScore++;
+            
+            computerTotalScore += computerScore;
+            
+            console.log("Your selection is :" + playerSelection + " Computer Selection is : " + computerSelection + " So, You Lose!");
+          } else if(playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Paper" && computerSelection === "Rock" || playerSelection === "Scissors" && computerSelection === "Paper"){
+            playerScore++;
   
+            playerTotalScore += playerScore;
+  
+            console.log("Your selection is :" + playerSelection + " Computer Selection is : " + computerSelection +  " So, You Win!");
+          } 
+          else if(playerSelection !== "Rock" || playerSelection !== "Paper" ||  playerSelection !== "Scissors"){
+              prompt("Please enter the words of 'Rock' or 'Paper' or 'Scissors'.\nUppercase Lowercase Capitalize all OK. \n But another words don't accept the game.");        
+          } 
 
-const playerSelection = playerPlay();
-const computerSelection = computerPlay();
-playRound(playerSelection, computerSelection);
+        }
+      playRound(playerSelection, computerSelection);
+    } 
 
-function playerPlay() {
-  const playerChoice =  prompt("Hello, world.\nThis is Rock Paper Scissors Game on the console. \nPlease enter the words of 'Rock' or 'Paper' or 'Scissors'. \nUppercase Lowercase Capitalize all OK");
+  game(); 
+  console.log("playerScore is : " + playerTotalScore);
+  console.log("computerScore is : " + computerTotalScore);
+  
+  game(); 
+  console.log("playerScore is : " + playerTotalScore);
+  console.log("computerScore is : " + computerTotalScore);
+  
+  game(); 
+  console.log("playerScore is : " + playerTotalScore);
+  console.log("computerScore is : " + computerTotalScore);
+  
+  game(); 
+  console.log("playerScore is : " + playerTotalScore);
+  console.log("computerScore is : " + computerTotalScore);
+  
+  game(); 
+  console.log("playerScore is : " + playerTotalScore);
+  console.log("computerScore is : " + computerTotalScore);
+  
+  game(); 
+  console.log("TotalplayerScore is : " + playerTotalScore);
+  console.log("TotalcomputerScore is : " + computerTotalScore);
 
-  return capitalizeWords(playerChoice);
-}
+  if (playerTotalScore > computerTotalScore) {
+    console.log("Congratulation! You win!");
+  } else if (playerTotalScore === computerTotalScore) {
+    console.log("Oh, You Draw!");
+  } else if (playerTotalScore < computerTotalScore) {
+    console.log("Oh, sorry. You lose...");
+  }
 
-function computerPlay() {
-  const computerChoice = ['Rock', 'Paper', 'Scissors'];
-  return computerChoice[Math.floor(Math.random()*computerChoice.length)];
-}
-
-function capitalizeWords(str) {
-  let getCapitalizedSentence = str[0].toUpperCase() + str.slice(1).toLowerCase();
-  return getCapitalizedSentence;
-}
+  alert("Game is over. Thank you for playing!");
