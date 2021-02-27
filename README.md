548ef5355982a06dd2aedc6f5368bc44ec28f0a1
Zan Hiro <zanhiro160@gmail.com>
2021-02-27 19:26:11 +0900

    Finished Project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ROCK-PAPER-SCISSORS
 This is for The Odin Project Homework.
+
+Simple Rock Paper Scissors Game.
+
+If player or computer has 5 points, game is over and points reset.
+
+Just simple.
+
+But I created by myself.(I don't watch youtube or any resourses.)
+
+Thank you.
--- javascript/game.js
+++ javascript/game.js
@@ -18,11 +18,11 @@
   const playerSelection = playerChoice;
 
   //Play game function
-  function Game() {
-    window.addEventListener('click', e => {
+  function playRound() {
 
-      if(e.target.className === "container") return;
-    
+    //get Click Event 
+    window.addEventListener('click', e => {
+      if(e.target.className === "container") return;    
       const buttons = document.querySelector(`.${e.target.className}`);
   
       if(buttons.className === "btnrock") {
@@ -36,60 +36,73 @@
         yourChoice.innerText = playerChoice;
       }
 
-      const computerChoice = ['Rock', 'Paper', 'Scissors'];
-      const computerSelection = computerChoice[Math.floor(Math.random()*computerChoice.length)];
-      computerSelect.innerText = computerSelection;
+      computer();
 
-      if((yourChoice.innerText === "Rock" && computerSelect.innerText === "Rock") || (yourChoice.innerText === "Paper" && computerSelect.innerText === "Paper") || (yourChoice.innerText === "Scissors" && computerSelect.innerText === "Scissors" )) {
-        playerScore += 0;
-        computerScore += 0;
+      win();
 
-        yourScoreDiv.innerText = playerScore;
-        computerScoreDiv.innerText = computerScore;
+      draw();
 
-        gameResult.style.color = "green";
-        gameResult.innerText = "Game is Draw!";
+      lost();
 
-      } 
-      
-      if(yourChoice.innerText === "Rock" && computerSelect.innerText === "Paper" || yourChoice.innerText === "Paper" && computerSelect.innerText === "Scissors" || yourChoice.innerText === "Scissors" && computerSelect.innerText === "Rock") {
-      
-      computerScore++;
-
-      yourScoreDiv.innerText = playerScore;
-      computerScoreDiv.innerText = computerScore;
-    
-      computerTotalScore = computerScore;
-
-      gameResult.style.color = 'red'; 
-      gameResult.innerText = "You Lose!";
-      } 
-      
-      if(yourChoice.innerText === "Rock" && computerSelect.innerText === "Scissors" || yourChoice.innerText === "Paper" && computerSelect.innerText === "Rock" || yourChoice.innerText === "Scissors" && computerSelect.innerText === "Paper"){
-      playerScore++;
-    
-      playerTotalScore = playerScore;
-
-      yourScoreDiv.innerText = playerScore;
-      computerScoreDiv.innerText = computerScore;
-
-      gameResult.style.color = "blue";
-      gameResult.innerText = "You Win!";
-    }
-
-    if(playerTotalScore === 5 || computerTotalScore === 5) {
-      if (playerTotalScore === 5) {
-        gameResult.style.color = "blue";
-        gameResult.innerText = "Game is Over. Congratulation! You win!";
-      } else if (computerTotalScore === 5) {
-        gameResult.style.color = "red";
-        gameResult.innerText = "Game is Over. Sorry. You lose...";
-      }
-      resetScore();
-    }
+      result();    
   });
 }
 
+playRound();
+
+//Computer selection(rock or paper or scissors)
+function computer() {
+  const computerChoice = ['Rock', 'Paper', 'Scissors'];
+  const computerSelection = computerChoice[Math.floor(Math.random()*computerChoice.length)];
+  computerSelect.innerText = computerSelection;
+}
+
+//In the case Win, DOM Manipulation
+function win() {
+  if(yourChoice.innerText === "Rock" && computerSelect.innerText === "Scissors" || yourChoice.innerText === "Paper" && computerSelect.innerText === "Rock" || yourChoice.innerText === "Scissors" && computerSelect.innerText === "Paper"){
+    playerScore++;
+  
+    playerTotalScore = playerScore;
+
+    yourScoreDiv.innerText = playerScore;
+    computerScoreDiv.innerText = computerScore;
+
+    gameResult.style.color = "blue";
+    gameResult.innerText = "You Win!";
+  }
+}
+
+//In the case Draw, DOM Manipulation
+function draw() {
+  if((yourChoice.innerText === "Rock" && computerSelect.innerText === "Rock") || (yourChoice.innerText === "Paper" && computerSelect.innerText === "Paper") || (yourChoice.innerText === "Scissors" && computerSelect.innerText === "Scissors" )) {
+    playerScore += 0;
+    computerScore += 0;
+
+    yourScoreDiv.innerText = playerScore;
+    computerScoreDiv.innerText = computerScore;
+
+    gameResult.style.color = "green";
+    gameResult.innerText = "Game is Draw!";
+  } 
+}
+
+//In the case Lost, DOM Manipulation
+function lost() {
+  if(yourChoice.innerText === "Rock" && computerSelect.innerText === "Paper" || yourChoice.innerText === "Paper" && computerSelect.innerText === "Scissors" || yourChoice.innerText === "Scissors" && computerSelect.innerText === "Rock") {
+
+    computerScore++;
+
+    yourScoreDiv.innerText = playerScore;
+    computerScoreDiv.innerText = computerScore;
+  
+    computerTotalScore = computerScore;
+
+    gameResult.style.color = 'red'; 
+    gameResult.innerText = "You Lose!";
+    } 
+}
+
+//After games, reset score
 function resetScore() {
   playerScore = 0;
   computerScore = 0;
@@ -97,4 +110,16 @@ function resetScore() {
   computerTotalScore = 0;
 }
 
-  Game();
+//After games, display result
+function result() {
+  if(playerTotalScore === 5 || computerTotalScore === 5) {
+    if (playerTotalScore === 5) {
+      gameResult.style.color = "blue";
+      gameResult.innerText = 'Game is Over. Congratulation! You win!';
+    } else if (computerTotalScore === 5) {
+      gameResult.style.color = "red";
+      gameResult.innerText = "Game is Over. Sorry. You lose...";
+    }
+    resetScore();
+  } 
+}
